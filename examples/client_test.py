af8b4bf2a96da763ec39779708c980c3d8ebf0f3
Nitrox <thomas@esculpavit.com>
2025-04-07 21:11:36 +0200

    modifing examples to suite the new connection.py

--- examples/client_test.py
+++ examples/client_test.py
@@ -3,5 +3,5 @@ from connection import SecureConnection
 socket = SecureConnection(host="localhost", port=43221, verbose=False) # Replace with your server's IP and port (verbose=True for debugging)
 socket.connect() # connect to the server and and securely communicate a AES and HMAC keys
 socket.send(b"Hello World") # send encrypted message
-socket.send(b"Hello World", encrypted=False) # send unencrypted message
+print(socket.recv()) # receive encrypted message
 socket.disconnect() # disconnect from the server
--- examples/server_test.py
+++ examples/server_test.py
@@ -3,6 +3,11 @@ from connection import SecureConnection
 socket = SecureConnection(host="localhost", port=43221, verbose=True) # Replace with your server's IP and port
 socket.start_server() # start the server
 socket.accept_client() # wait for a client to connect
-print(socket.recv()) # receive encrypted message
-print(socket.recv(False)) # receive unencrypted message
-socket.disconnect() # disconnect from the client
+socket.accept_client() # wait for a client to connect
+for client in socket.clients:
+    print(client) # print the client connection information
+    socket.send(b"Hello World", client=client) # send encrypted message to all clients
+socket.show()
+# print(socket.recv()) # receive encrypted message
+# print(socket.recv(encrypted=False)) # receive unencrypted message
+# socket.disconnect() # disconnect from the client
