cf4bbc18474815d541c8bb4ae3c5988809cff8cf
Nitrox <thomas@esculpavit.com>
2025-04-07 21:08:23 +0200

    Changing the way the client was handled in the server side.

--- connection.py
+++ connection.py
@@ -21,6 +21,17 @@ A simple HMAC mecanism is used to ensure the integrity and autentication of the 
 
 """
 
+class Client: # Client class to handle client management in the server
+    def __init__(self, socket):
+        self.ip = socket.getpeername()[0]
+        self.port = socket.getpeername()[1]
+        self.socket = socket
+        self.aes_key = None
+        self.hmac_key = None
+        
+    def __repr__(self):
+        return f"client {self.ip}:{self.port} --- aes_key: {self.aes_key.hex() if self.aes_key else 'None'} --- hmac_key: {self.hmac_key.hex() if self.hmac_key else 'None'}"
+
 class SecureConnection:
     def __init__(self, host='localhost', port=12345, verbose=False):
         self.host = host
@@ -30,6 +41,7 @@ class SecureConnection:
         
         self.is_server = False
         self.is_client = False
+        self.clients: list = []  # list of client sockets for the server
         
         self.private_key = None
         self.public_key = None
@@ -41,10 +53,15 @@ class SecureConnection:
         self.private_key = RSA.generate(size)
         self.public_key = self.private_key.public_key()
         if save:
-            with open("private.pem", "wb") as f: # todo : use encryption for private key
-                f.write(self.private_key.export_key())
+            with open("private.pem", "wb") as f:
+                print("Choose a secure passphrase to protect your private key: ")
+                password = input()
+                print(f"saving private key... with passphrase '{password}'")
+                f.write(self.private_key.export_key(passphrase=password, pkcs=8, protection="scryptAndAES128-CBC")) # change passphrase
+                print("Private key saved as 'private.pem'.")
             with open("public.pem", "wb") as f:
-                f.write(self.public_key.export_key())
+                f.write(self.public_key.export_key(format="PEM")) # change passphrase
+                print("Public key saved as 'public.pem'.")
 
     def load_rsa_key(self, private_key_path=None, public_key_path=None):
         """Load RSA keys from files."""
@@ -55,37 +72,6 @@ class SecureConnection:
             with open(public_key_path, "rb") as f:
                 self.public_key = RSA.import_key(f.read())
         print("RSA keys loaded.")
-
-    def show(self):
-        """Display the current connection status."""
-        print("Connection Status:")
-        print(f"Host: {self.host}")
-        print(f"Port: {self.port}")
-        print(f"Socket: {self.socket}")
-        print(f"Is Server: {self.is_server}")
-        print(f"Is Client: {self.is_client}")
-        
-        if self.is_server:
-            print(f"Server is running on {self.host}:{self.port}")
-        elif self.is_client:
-            print(f"Client is connected to {self.host}:{self.port}")
-        else:
-            print("No connection established.")
-        
-        if self.private_key:
-            print("Private Key:")
-            print(self.private_key.export_key().decode())
-            print(getsizeof(self.private_key))
-        if self.public_key:
-            print("public Key:")
-            print(self.public_key.export_key().decode())
-            print(getsizeof(self.public_key))
-        
-        if self.socket:
-            print("Socket Options:")
-            print(self.socket.getsockname())  # local address
-            print(self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF))  # in bytes
-            print(self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF))
 
     def connect(self):
         """Establish a connection to the server."""
@@ -119,16 +105,16 @@ class SecureConnection:
         if self.verbose: print(f"Sent client salt: {self.client_salt.hex()}") 
 
         # ---- key derivation ----
-        self.key_derivation(main_key)  # derive a key from the AES key and server's salt
-        # todo receive server's public key
-        # todo send encryption master key to server
+        self.key_derivation(main_key)  # derive a key from the AES key and server's saltr
     
     def disconnect(self) -> None:
         """Disconnect the client."""
         if self.socket:
-            self.socket.shutdown(socket.SHUT_RDWR)
+            try:
+                self.socket.shutdown(socket.SHUT_RDWR)
+            except OSError:
+                pass  # Socket already closed
             self.socket.close()
-            print("Disconnected from server.")
         self.socket = None
 
     def start_server(self) -> None:
@@ -146,32 +132,38 @@ class SecureConnection:
             return
         
         #---- accept a client connection ----
-        self.socket, addr = self.socket.accept()
+        client_socket, addr = self.socket.accept() # accept the client connection
         print(f"Connection accepted from {addr}")
-        
+        current_client = Client(client_socket)  # create a client object
+        self.clients.append(current_client)  # add the client socket to the list of client sockets
         # ---- sending salt and public key to the client ----
-        self.send(self.public_key.export_key(format="DER"), encrypted=False)  # send public key to client    
+        self.send(self.public_key.export_key(format="DER"), client=current_client, encrypted=False)  # send public key to client    
         self.server_salt = get_random_bytes(8)  # generate a random salt for the server
-        self.send(self.server_salt, encrypted=False)
+        self.send(self.server_salt, client=current_client, encrypted=False)  # send server's salt to client
         if self.verbose: print(f"Sent server's salt: {self.server_salt.hex()}")
-        
+
         # ---- receiving data from the client ----
-        r_key_encrypted = self.recv(encrypted=False)  # receive main_key from client
+        r_key_encrypted = self.recv(encrypted=False, client=current_client)  # receive main_key from client
         if self.verbose: print(r_key_encrypted.hex())
         main_key = self.decrypt_rsa(r_key_encrypted)  # decrypt the main_key with private key
         if self.verbose: print(f"main key: {main_key.hex()}")
-        self.client_salt = self.recv(encrypted=False)  # receive client salt
+        self.client_salt = self.recv(encrypted=False, client=current_client)  # receive client salt
         if self.verbose: print(f"Received client salt: {self.client_salt.hex()}")
         
         # ---- key derivation ----
         self.key_derivation(main_key)  # derive a key from the AES key and server's salt
-        
+        current_client.aes_key = self.aes_key
+        current_client.hmac_key = self.hmac_key
+        return
+
     def key_derivation(self, main_key:bytes) -> bytes:
-        main_key: bytes = scrypt(main_key, self.server_salt + self.client_salt, 64, N=2**14, r=8, p=1)  # derive a key from the AES key and server's salt
-        self.aes_key = main_key[:32]  # use the first 16 bytes as the AES key
-        self.hmac_key = main_key[32:]  # use the last 16 bytes as the HMAC key
+        salt = self.server_salt + self.client_salt  # Ensure fixed lengths for salts
+        main_key: bytes = scrypt(main_key, salt, 64, N=2**14, r=8, p=1)  # derive a key from the AES key and server's salt
+        self.aes_key = main_key[:32]  # AES-256 key (32 bytes)
+        self.hmac_key = main_key[32:]  # use the last 32 bytes as the HMAC key
         if self.verbose: print(f"AES key: {self.aes_key.hex()}")
         if self.verbose: print(f"HMAC key: {self.hmac_key.hex()}")
+        return self.aes_key, self.hmac_key
         
     def stop_server(self) -> None:
         """Stop the server."""
@@ -181,43 +173,76 @@ class SecureConnection:
         self.socket = None
         self.is_server = False
         
-    def recv(self, encrypted=True) -> bytes:
+    def recv(self, encrypted=True, client: Client=None) -> bytes:
         """Receive a message from the client."""
-        if self.socket:
-            header = self.socket.recv(4)
-            if not header:
-                return None
-            message_length = int.from_bytes(header, 'big')
-            data = b""
-            while len(data) < message_length:
-                # Receive the message in chunks to avoid blocking and to handle large messages
-                size = min(2048, message_length - len(data))
-                chunk = self.socket.recv(size)
-                if not chunk: break
-                data += chunk
-            
-            if len(data) == message_length:
+        if not self.is_server and not self.is_client: # if not connected at all
+            return None
+
+        if client is None: # if no client is specified, use the last client socket
+            if self.is_server:
+                client_socket = self.clients[-1].socket if self.clients else None  # get the last client socket
+                aes_key, hmac_key = self.clients[-1].aes_key, self.clients[-1].hmac_key
+            elif self.is_client:
+                client_socket = self.socket
+                aes_key, hmac_key = self.aes_key, self.hmac_key
+        else:
+            client_socket = client.socket # use the specified client socket
+            aes_key, hmac_key = client.aes_key, client.hmac_key
+
+        if client_socket:
+            try:
+                header = client_socket.recv(4)
+                if not header:
+                    raise ConnectionError("Socket closed or no data received.")
+                message_length = int.from_bytes(header, 'big')
+                data = b""
+                while len(data) < message_length:
+                    size = min(2048, message_length - len(data))
+                    chunk = client_socket.recv(size)
+                    if not chunk:
+                        raise ConnectionError("Socket closed during message reception.")
+                    data += chunk
+
+                if len(data) != message_length:
+                    raise ValueError("Incomplete message received.")
+
                 if encrypted:
-                    # Decrypt the message using AES and HMAC
-                    data = self.decrypt_aes_and_hmac(data)
+                    data = self.decrypt_aes_and_hmac(data, aes_key, hmac_key)
                 return data
-            else:
-                print("Incomplete message received.")
+            except (ConnectionError, ValueError) as e:
+                print(f"Error receiving data: {e}")
                 return None
         return None
     
-    def send(self, message:bytes, encrypted=True) -> None:
+    def send(self, message:bytes, encrypted=True, client:Client=None) -> None:
         """Send a message to the client."""
-
-        if self.socket:
+        if not self.is_server and not self.is_client: # if not connected at all
+            return
+        
+        if client is None:
+            if self.is_server: # if server, send to the last client socket
+                if len(self.clients) > 0:
+                    client_socket = self.clients[-1].socket
+                    aes_key, hmac_key = self.clients[-1].aes_key, self.clients[-1].hmac_key
+                else:
+                    print("No connected clients.")
+                    return
+            elif self.is_client: # if client, send to the server socket
+                client_socket = self.socket
+                aes_key, hmac_key = self.aes_key, self.hmac_key
+        else:
+            client_socket = client.socket
+            aes_key, hmac_key = client.aes_key, client.hmac_key
+        
+        if client_socket:
             if encrypted:
                 # Encrypt the message using AES and HMAC
-                message = self.encrypt_aes_and_hmac(message)
+                message = self.encrypt_aes_and_hmac(message, aes_key, hmac_key)
                 header = len(message).to_bytes(4, 'big')
             else:
                 header = len(message).to_bytes(4, 'big')
                 
-            self.socket.sendall(header + message)
+            client_socket.sendall(header + message)
 
         else:
             print("No connected.")
@@ -238,20 +263,20 @@ class SecureConnection:
         decrypted_message = cipher.decrypt(message)
         return decrypted_message
 
-    def encrypt_aes_and_hmac(self, message:bytes) -> bytes:
+    def encrypt_aes_and_hmac(self, message:bytes, aes_key:bytes, hmac_key:bytes) -> bytes:
         """Encrypt a message using AES and HMAC."""
-        if not self.aes_key or not self.hmac_key: 
+        if not aes_key or not hmac_key: 
             raise ValueError("AES key or HMAC key is not available.")
         
         # ---- AES encryption ----
-        cipher = AES.new(self.aes_key, AES.MODE_CBC) # AES encryption
+        cipher = AES.new(aes_key, AES.MODE_CBC) # AES encryption
         iv = cipher.iv
         ciphertext = cipher.encrypt(pad(message, AES.block_size)) # AES encryption
-        hmac = HMAC.new(self.hmac_key, ciphertext, SHA256).digest()
+        hmac = HMAC.new(hmac_key, ciphertext, SHA256).digest()
         #print(iv.hex(), ciphertext.hex(), hmac.hex())
         return iv + ciphertext + hmac
-    
-    def decrypt_aes_and_hmac(self, message:bytes) -> bytes:
+
+    def decrypt_aes_and_hmac(self, message:bytes, aes_key:bytes, hmac_key:bytes) -> bytes:
         """Decrypt a message using AES and HMAC.
         This function will also verify the HMAC to ensure the integrity of the message.
         
@@ -269,21 +294,57 @@ class SecureConnection:
         ciphertext = message[AES.block_size:-32]
         hmac = message[-32:]
         # ---- HMAC verification ----
-        h = HMAC.new(self.hmac_key, ciphertext, SHA256)
+        h = HMAC.new(hmac_key, ciphertext, SHA256)
         try:
             h.verify(hmac)
         except ValueError:
-            print("HMAC verification failed.") # info
-            return None
+           raise ValueError("HMAC verification failed.")
         
         # ---- AES decryption ----
-        cipher = AES.new(self.aes_key, AES.MODE_CBC, iv)
+        cipher = AES.new(aes_key, AES.MODE_CBC, iv)
         try:
             decrypted_message = unpad(cipher.decrypt(ciphertext), AES.block_size)
             return decrypted_message
         except ValueError:
-            print("Decryption failed.")
-            return None
+            raise ValueError("Decryption failed.")
         
     def __repr__(self):
-        self.show()
+        return f"SecureConnection(aes_key={self.aes_key.hex() if self.aes_key else None}, hmac_key={self.hmac_key.hex() if self.hmac_key else None})"
+     
+    def show(self, verbose=False) -> None:
+        """Display the current connection status."""
+        print("Connection Status:")
+        print(f"Host: {self.host}")
+        print(f"Port: {self.port}")
+        print(f"Socket: {self.socket}")
+        print(f"Is Server: {self.is_server}")
+        print(f"Is Client: {self.is_client}")
+        
+        if self.is_server:
+            print(f"Server is running on {self.host}:{self.port}")
+            if self.clients:
+                print(f"Connected clients:")
+                for client in self.clients:
+                    print(f"- {client}")
+        elif self.is_client:
+            print(f"Client is connected to {self.host}:{self.port}")
+        else:
+            print("No connection established.")
+        
+        if self.private_key and verbose:
+            print("Private Key:")
+            print(self.private_key.export_key().decode())
+            print(getsizeof(self.private_key))
+        if self.public_key and verbose:
+            print("Public Key:")
+            print(self.public_key.export_key().decode())
+            print(getsizeof(self.public_key))
+        
+        if self.socket:
+            print("Socket Options:")
+            print(self.socket.getsockname())  # local address
+            print(self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF))  # in bytes
+            print(self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF))
+
+
+
